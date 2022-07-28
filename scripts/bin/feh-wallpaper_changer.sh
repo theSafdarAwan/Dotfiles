3e343c5d20ba15d2c6e941ee1ec51b5a42280690
TheSafdarAwan <thesafdarawan@gmail.com>
2022-07-28 22:00:44 +0500

    add the shell script to randomly change the wallpaper after every 5 min

---
+++ scripts/bin/feh-wallpaper_changer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+pidof -x feh-wallpaper_changer.sh | Xorg pkill
+
+time=5m
+dir=~/wallpapers
+
+cd $dir
+
+function comprobar() {
+num=1
+for x in *;
+do
+       let num=$num+1
+done
+}
+
+while [ 1 ]; do
+        comprobar
+	var_ran=$(($RANDOM%$num))
+        var_aum=1
+	for y in *; do
+		if [ "$var_ran" -eq "$var_aum" ]; then
+			feh --bg-fill $y
+			sleep $time
+			break
+		fi		
+		
+		let var_aum=$var_aum+1
+	done
+done
