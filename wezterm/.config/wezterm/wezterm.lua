3cf14a634ee57e139c9a580ffcc0e31229cb4c8d
TheSafdarAwan <thesafdarawan@gmail.com>
2022-08-15 18:31:22 +0500

    add fira code nerd patched fonts

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,13 +1,22 @@
-local dracula = require 'dracula';
+local wezterm = require("wezterm")
+local dracula = require("dracula")
+
 return {
-    colors = dracula,
-    tab_bar_at_bottom = true,
-    use_fancy_tab_bar = false,
-    hide_tab_bar_if_only_one_tab = true,
+	colors = dracula,
+	tab_bar_at_bottom = true,
+	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
 
-    window_background_opacity = 0.99,
+	window_background_opacity = 0.99,
 
-    font_size = 12.0,
+	font_size = 11.0,
+	font = wezterm.font("FiraCode Nerd Font", { weight = "Medium", italic = false }),
 
-    default_cursor_style = 'BlinkingBar',
+	default_cursor_style = "BlinkingBar",
+	window_padding = {
+		left = 2,
+		right = 2,
+		top = 0,
+		bottom = 0,
+	},
 }
