b4a685638c02c137667fb56af8dc19a34a9a5280
TheSafdarAwan <thesafdarawan@gmail.com>
2022-08-21 20:58:34 +0500

    update wezterm conf and add new theme for rofi

--- wezterm/.config/wezterm/init.lua
+++ wezterm/.config/wezterm/init.lua
@@ -1,28 +1,152 @@
 local wezterm = require("wezterm")
 local dracula = require("dracula")
 
+local font_name = "FiraCode Nerd Font"
+local function font_with_fallback(name, params)
+    local names = { name, "Apple Color Emoji", "azuki_font" }
+    return wezterm.font_with_fallback(names, params)
+end
+
 return {
-	colors = dracula,
+    colors = dracula,
 
-	tab_bar_at_bottom = true,
-	use_fancy_tab_bar = false,
-	hide_tab_bar_if_only_one_tab = true,
-    -- true_colors = false,
+    front_end = "OpenGL",
+    enable_wayland = false,
 
-	window_background_opacity = 1.0,
-	audible_bell = "Disabled",
+    tab_bar_at_bottom = true,
+    use_fancy_tab_bar = false,
+    hide_tab_bar_if_only_one_tab = true,
+    show_tab_index_in_tab_bar = false,
+    inactive_pane_hsb = { saturation = 1.0, brightness = 0.8 },
+    window_frame = { active_titlebar_bg = "#090909", font = font_with_fallback(font_name, { bold = true }) },
 
-	font_size = 11.0,
-	font = wezterm.font("FiraCode Nerd Font", { weight = "Medium", italic = false }),
+    window_background_opacity = 1.0,
+    audible_bell = "Disabled",
 
-	cursor_blink_rate = 0,
-	line_height = 1,
+    font_size = 11.0,
+    -- Font config
+    font = font_with_fallback(font_name),
+    -- font = wezterm.font("", { weight = "Medium", italic = false }),
+    font_rules = {
+        {
+            italic = true,
+            font = font_with_fallback(font_name, { italic = true }),
+        },
+        {
+            italic = true,
+            intensity = "Bold",
+            font = font_with_fallback(font_name, { italic = true, bold = true }),
+        },
+        {
+            intensity = "Bold",
+            font = font_with_fallback(font_name, { bold = true }),
+        },
+    },
 
-	window_padding = {
-		left = 2,
-		right = 2,
-		top = 0,
-		bottom = 0,
-	},
+    -- default_cursor_style = "BlinkingUnderline",
+    cursor_blink_rate = 0,
+    line_height = 1,
+
+    window_padding = {
+        left = 2,
+        right = 2,
+        top = 0,
+        bottom = 0,
+    },
     check_for_updates = false,
+    warn_about_missing_glyphs = false,
+    window_close_confirmation = "NeverPrompt",
+
+    disable_default_key_bindings = true,
+    keys = {
+        {
+            key = "v",
+            mods = "CTRL|ALT",
+            action = wezterm.action({
+                SplitHorizontal = { domain = "CurrentPaneDomain" },
+            }),
+        },
+        {
+            key = "s",
+            mods = "CTRL|ALT",
+            action = wezterm.action({
+                SplitVertical = { domain = "CurrentPaneDomain" },
+            }),
+        },
+        {
+            key = "q",
+            mods = "CTRL",
+            action = wezterm.action({ CloseCurrentPane = { confirm = false } }),
+        },
+        {
+            key = "h",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action({ ActivatePaneDirection = "Left" }),
+        },
+        {
+            key = "l",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action({ ActivatePaneDirection = "Right" }),
+        },
+        {
+            key = "k",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action({ ActivatePaneDirection = "Up" }),
+        },
+        {
+            key = "j",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action({ ActivatePaneDirection = "Down" }),
+        },
+        {
+            key = "h",
+            mods = "CTRL|SHIFT|ALT",
+            action = wezterm.action({ AdjustPaneSize = { "Left", 1 } }),
+        },
+        {
+            key = "l",
+            mods = "CTRL|SHIFT|ALT",
+            action = wezterm.action({ AdjustPaneSize = { "Right", 1 } }),
+        },
+        {
+            key = "k",
+            mods = "CTRL|SHIFT|ALT",
+            action = wezterm.action({ AdjustPaneSize = { "Up", 1 } }),
+        },
+        {
+            key = "j",
+            mods = "CTRL|SHIFT|ALT",
+            action = wezterm.action({ AdjustPaneSize = { "Down", 1 } }),
+        },
+        { -- browser-like bindings for tabbing
+            key = "t",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }),
+        },
+        {
+            key = "Tab",
+            mods = "CTRL",
+            action = wezterm.action({ ActivateTabRelative = 1 }),
+        },
+        {
+            key = "Tab",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action({ ActivateTabRelative = -1 }),
+        }, -- standard copy/paste bindings
+        {
+            key = "x",
+            mods = "CTRL",
+            action = "ActivateCopyMode",
+        },
+        {
+            key = "v",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action({ PasteFrom = "Clipboard" }),
+        },
+        {
+            key = "c",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action({ CopyTo = "ClipboardAndPrimarySelection" }),
+        },
+    },
 }
