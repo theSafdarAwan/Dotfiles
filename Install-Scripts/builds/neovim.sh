5a94184e0fbde999b320580db8a3fcab968cb474
TheSafdarAwan <thesafdarawan@gmail.com>
2022-11-12 15:21:38 +0500

    Install Script: change the nvim install script

--- Install-Scripts/builds/neovim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm -rf ~/.local/temp/builds/
-mkdir -p ~/.local/temp/builds/
-cd ~/.local/temp/builds/
-git clone https://github.com/neovim/neovim
-cd neovim
-make CMAKE_BUILD_TYPE=RelWithDebInfo
-sudo make install 
-sudo mv .local/temp/builds/neovim/build/bin/nvim /usr/bin/
