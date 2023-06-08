9c007cd26a771696a4bf0d387ce88881647e9567
TheSafdarAwan <thesafdarawan@gmail.com>
2023-06-08 18:04:09 +0500

    cfg(emacs): add icons

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -10,7 +10,7 @@
  '(custom-safe-themes
    '("fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" "7887cf8b470098657395502e16809523b629249060d61607c2225d2ef2ad59f5" default))
  '(package-selected-packages
-   '(all-the-icons-dired all-the-icons dired-single single-dired polymode org-brain solarized-theme doom-themes atom-one-dark-theme dracula-theme modus-themes vterm magit org-bullets org-roam-ui org-roam pomidor dired-icon undo-tree helpful rainbow-delimiters markdown-mode lua-mode embark orderless company ivy-rich counsel perspective evil-surround evil hydra ivy-posframe use-package)))
+   '(all-the-icons-ivy all-the-icons-completion all-the-icons-dired all-the-icons dired-single single-dired polymode org-brain solarized-theme doom-themes atom-one-dark-theme dracula-theme modus-themes vterm magit org-bullets org-roam-ui org-roam pomidor dired-icon undo-tree helpful rainbow-delimiters markdown-mode lua-mode embark orderless company ivy-rich counsel perspective evil-surround evil hydra ivy-posframe use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
