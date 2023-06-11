cb390922c129eb38b09cd3c8d1a969b2a0d13add
TheSafdarAwan <thesafdarawan@gmail.com>
2023-06-11 09:48:38 +0500

    cfg(emacs): update

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,1 +1,14 @@
 (org-babel-load-file (expand-file-name "setup/init.org" user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(anki-editor no-littering vterm use-package undo-tree solarized-theme rainbow-delimiters pomidor polymode perspective org-roam-ui org-bullets org-brain orderless modus-themes markdown-mode magit lua-mode ivy-posframe hydra helpful evil-surround embark dracula-theme doom-themes dired-single dired-icon dired-hide-dotfiles counsel company auto-package-update atom-one-dark-theme all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-dired all-the-icons-completion)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
