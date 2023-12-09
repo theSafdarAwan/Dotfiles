6738845bd91295ea29c70e88d7919ac8a752968d
TheSafdarAwan <thesafdarawan@gmail.com>
2023-12-09 18:01:20 +0500

    config(emacs): updated emacs config

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,5 +1,7 @@
 (org-babel-load-file (expand-file-name "setup/init.org" user-emacs-directory))
+
 (custom-set-variables
+ 
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
@@ -9,7 +11,7 @@
  '(org-agenda-files
    '("/home/safdar/safdar-local/org-roam-notes/testing tags.org"))
  '(package-selected-packages
-   '(anki-editor no-littering vterm use-package undo-tree solarized-theme rainbow-delimiters pomidor polymode perspective org-roam-ui org-bullets org-brain orderless modus-themes markdown-mode magit lua-mode ivy-posframe hydra helpful evil-surround embark dracula-theme doom-themes dired-single dired-icon dired-hide-dotfiles counsel company auto-package-update atom-one-dark-theme all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-dired all-the-icons-completion)))
+   '(org-modern anki-editor no-littering vterm use-package undo-tree solarized-theme rainbow-delimiters pomidor polymode perspective org-roam-ui org-bullets org-brain orderless modus-themes markdown-mode magit lua-mode ivy-posframe hydra helpful evil-surround embark dracula-theme doom-themes dired-single dired-icon dired-hide-dotfiles counsel company auto-package-update atom-one-dark-theme all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-dired all-the-icons-completion)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
