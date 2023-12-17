7f32bb96b80bbcaebe9e88865ad939641f8d4d37
TheSafdarAwan <thesafdarawan@gmail.com>
2023-12-17 23:49:13 +0500

    cfg(emacs): updated config

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,17 +1,16 @@
 (org-babel-load-file (expand-file-name "setup/init.org" user-emacs-directory))
 
 (custom-set-variables
- 
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("21e3d55141186651571241c2ba3c665979d1e886f53b2e52411e9e96659132d4" "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" "680f62b751481cc5b5b44aeab824e5683cf13792c006aeba1c25ce2d89826426" "c517e98fa036a0c21af481aadd2bdd6f44495be3d4ac2ce9d69201fcb2578533" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" default))
- '(org-agenda-files
-   '("/home/safdar/safdar-local/org-roam-notes/testing tags.org"))
+ '(org-agenda-files '("~/safdar-local/org-roam-notes/org.org"))
+ '(org-directory "~/safdar-local/org-roam-notes/")
  '(package-selected-packages
-   '(org-modern anki-editor no-littering vterm use-package undo-tree solarized-theme rainbow-delimiters pomidor polymode perspective org-roam-ui org-bullets org-brain orderless modus-themes markdown-mode magit lua-mode ivy-posframe hydra helpful evil-surround embark dracula-theme doom-themes dired-single dired-icon dired-hide-dotfiles counsel company auto-package-update atom-one-dark-theme all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-dired all-the-icons-completion)))
+   '(org-ql org-modern-indent sqlite3 multiple-cursors org-modern anki-editor no-littering vterm use-package undo-tree solarized-theme rainbow-delimiters pomidor polymode perspective org-roam-ui org-bullets org-brain orderless modus-themes markdown-mode magit lua-mode ivy-posframe hydra helpful evil-surround embark dracula-theme doom-themes dired-single dired-icon dired-hide-dotfiles counsel company auto-package-update atom-one-dark-theme all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-dired all-the-icons-completion)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
